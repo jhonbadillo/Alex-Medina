70a15b3be815292ed031b431519f4b52f8fb33f6
jhonbadillo <jhonbadillo@hotmail.com>
2020-06-17 18:49:25 -0500

    Update _singlePost.vue

--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="post-page" class="page-wrapper post-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+    <site-hero :title="titulo" :subtitle="subtitulo" :image="Imagen">
       <span
         v-if="author && $siteConfig.posts.displayAuthor"
         class="author-wrapper"
