200996f1a7ae2410af44f175e93fb6517d059e60
jhonbadillo <jhonbadillo@hotmail.com>
2020-06-17 18:36:53 -0500

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
-    <site-hero title="Categorias" image="https://tinyurl.com/y94c8yo6" />
+    <site-hero title="Categories" image="https://tinyurl.com/y94c8yo6" />
     <main-section theme="one-column">
       <categories-grid />
     </main-section>
