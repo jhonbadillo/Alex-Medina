eea8e4631a42813a740cce7580194891eeb8075d
jhonbadillo <jhonbadillo@hotmail.com>
2020-06-17 21:20:24 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+    <site-hero :title="titulo" :subtitle="subtitulo" :image="Imagen">
       <button
         v-if="$siteConfig.newsletter.on"
         class="button is-primary"
