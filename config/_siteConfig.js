d67e967dc31e6d575a868d22870fe6db84b69e71
jhonbadillo <jhonbadillo@hotmail.com>
2020-06-18 04:21:38 -0500

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -74,7 +74,7 @@ export default {
     mailchimp: {
       on: true,
       formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
+        'http://eepurl.com/g7FCav'
     },
     custom: {
       on: false,
