607fa22d4232abd1fe2e44372f031b9bdb7e7da0
jhonbadillo <jhonbadillo@hotmail.com>
2020-06-18 22:00:48 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
       >
-        Suscribete a nuestro boletin
+        Suscribete a nuestro boletin.
       </button>
     </site-hero>
     <main-section theme="one-column">
